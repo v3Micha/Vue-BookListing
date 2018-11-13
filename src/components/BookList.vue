f0266846abc978e7aef36fe3c7fdf72f9e53a582
Michael A <v_3micha@hotmail.com>
2018-11-13 16:20:43 -0500

    Completed the module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>{{ title }} </h1>
+    <ul>
+      <li v-for="book in books">
+        {{ book.title }}:{{ book.author }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,6 +15,11 @@
     data() {
       return {
         title: 'All Books',
+        books: [
+          { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+          { title: 'American Gods', author: 'Neil Gaiman' },
+          { title: 'Amusing Ourselves to Death', author: 'Neil Postman' },
+        ],
       };
     },
   };
@@ -19,5 +29,15 @@
   h1,
   h2 {
     font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: block;
+    margin: 0 10px;
   }
 </style>
